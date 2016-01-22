1705901dd0df33a589e29998f79b432f16d4644d
fgiacomoni <franck.giacomoni@clermont.inra.fr>
2016-01-22 16:39:38 +0100

    Call new method to build out matrix with ids only.

--- wsdl_hmdb.pl
+++ wsdl_hmdb.pl
@@ -146,11 +146,13 @@ if ( ( defined $out_tab ) and ( defined $results ) ) {
 	if (defined $masses_file) {
 		my $lm_matrix = undef ;
 		if ( ( defined $nbline_header ) and ( $header_choice eq 'yes' ) ) {
-			$lm_matrix = $ocsv->set_lm_matrix_object('hmdb', $masses, $results ) ;
+#			$lm_matrix = $ocsv->set_lm_matrix_object('hmdb', $masses, $results ) ;
+			$lm_matrix = $ocsv->set_hmdb_matrix_object_with_ids('hmdb', $masses, $results ) ;
 			$lm_matrix = $ocsv->add_lm_matrix_to_input_matrix($complete_rows, $lm_matrix, $nbline_header-1) ;
 		}
 		elsif ( ( $header_choice eq 'no' ) ) {
-			$lm_matrix = $ocsv->set_lm_matrix_object(undef, $masses, $results ) ;
+#			$lm_matrix = $ocsv->set_lm_matrix_object(undef, $masses, $results ) ;
+			$lm_matrix = $ocsv->set_hmdb_matrix_object_with_ids(undef, $masses, $results ) ;
 			$lm_matrix = $ocsv->add_lm_matrix_to_input_matrix($complete_rows, $lm_matrix, 0) ;
 		}
 		$ocsv->write_csv_skel(\$out_tab, $lm_matrix) ;
